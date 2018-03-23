7f0d3a17e0e2a536d537fb7c060cdda10477f638
Jack <jckzhao@yahoo.com>
2018-03-23 02:35:10 -0400

    Add files via upload

---
+++ DiceRollerExtreme/DiceRollerExtreme/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace DiceRollerExtreme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxd4 = new System.Windows.Forms.TextBox();
+            this.textBoxd6 = new System.Windows.Forms.TextBox();
+            this.textBoxd8 = new System.Windows.Forms.TextBox();
+            this.textBoxd10 = new System.Windows.Forms.TextBox();
+            this.textBoxd12 = new System.Windows.Forms.TextBox();
+            this.textBoxd20 = new System.Windows.Forms.TextBox();
+            this.textBoxPercentile = new System.Windows.Forms.TextBox();
+            this.textBoxResults = new System.Windows.Forms.TextBox();
+            this.buttonRoll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "D4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "D6";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "D8";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "D10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "D12";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(62, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "D20";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(62, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Percentile";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Amount";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(49, 272);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Total";
+            // 
+            // textBoxd4
+            // 
+            this.textBoxd4.Location = new System.Drawing.Point(12, 50);
+            this.textBoxd4.Name = "textBoxd4";
+            this.textBoxd4.Size = new System.Drawing.Size(44, 20);
+            this.textBoxd4.TabIndex = 9;
+            this.textBoxd4.Text = "0";
+            // 
+            // textBoxd6
+            // 
+            this.textBoxd6.Location = new System.Drawing.Point(12, 76);
+            this.textBoxd6.Name = "textBoxd6";
+            this.textBoxd6.Size = new System.Drawing.Size(44, 20);
+            this.textBoxd6.TabIndex = 10;
+            this.textBoxd6.Text = "0";
+            // 
+            // textBoxd8
+            // 
+            this.textBoxd8.Location = new System.Drawing.Point(13, 102);
+            this.textBoxd8.Name = "textBoxd8";
+            this.textBoxd8.Size = new System.Drawing.Size(44, 20);
+            this.textBoxd8.TabIndex = 11;
+            this.textBoxd8.Text = "0";
+            // 
+            // textBoxd10
+            // 
+            this.textBoxd10.Location = new System.Drawing.Point(12, 129);
+            this.textBoxd10.Name = "textBoxd10";
+            this.textBoxd10.Size = new System.Drawing.Size(44, 20);
+            this.textBoxd10.TabIndex = 12;
+            this.textBoxd10.Text = "0";
+            // 
+            // textBoxd12
+            // 
+            this.textBoxd12.Location = new System.Drawing.Point(12, 155);
+            this.textBoxd12.Name = "textBoxd12";
+            this.textBoxd12.Size = new System.Drawing.Size(44, 20);
+            this.textBoxd12.TabIndex = 13;
+            this.textBoxd12.Text = "0";
+            // 
+            // textBoxd20
+            // 
+            this.textBoxd20.Location = new System.Drawing.Point(12, 181);
+            this.textBoxd20.Name = "textBoxd20";
+            this.textBoxd20.Size = new System.Drawing.Size(44, 20);
+            this.textBoxd20.TabIndex = 14;
+            this.textBoxd20.Text = "0";
+            // 
+            // textBoxPercentile
+            // 
+            this.textBoxPercentile.Location = new System.Drawing.Point(12, 208);
+            this.textBoxPercentile.Name = "textBoxPercentile";
+            this.textBoxPercentile.Size = new System.Drawing.Size(44, 20);
+            this.textBoxPercentile.TabIndex = 15;
+            this.textBoxPercentile.Text = "0";
+            // 
+            // textBoxResults
+            // 
+            this.textBoxResults.Enabled = false;
+            this.textBoxResults.Location = new System.Drawing.Point(16, 288);
+            this.textBoxResults.Name = "textBoxResults";
+            this.textBoxResults.Size = new System.Drawing.Size(100, 20);
+            this.textBoxResults.TabIndex = 16;
+            // 
+            // buttonRoll
+            // 
+            this.buttonRoll.Location = new System.Drawing.Point(28, 246);
+            this.buttonRoll.Name = "buttonRoll";
+            this.buttonRoll.Size = new System.Drawing.Size(75, 23);
+            this.buttonRoll.TabIndex = 17;
+            this.buttonRoll.Text = "Roll";
+            this.buttonRoll.UseVisualStyleBackColor = true;
+            this.buttonRoll.Click += new System.EventHandler(this.buttonRoll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(136, 322);
+            this.Controls.Add(this.buttonRoll);
+            this.Controls.Add(this.textBoxResults);
+            this.Controls.Add(this.textBoxPercentile);
+            this.Controls.Add(this.textBoxd20);
+            this.Controls.Add(this.textBoxd12);
+            this.Controls.Add(this.textBoxd10);
+            this.Controls.Add(this.textBoxd8);
+            this.Controls.Add(this.textBoxd6);
+            this.Controls.Add(this.textBoxd4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxd4;
+        private System.Windows.Forms.TextBox textBoxd6;
+        private System.Windows.Forms.TextBox textBoxd8;
+        private System.Windows.Forms.TextBox textBoxd10;
+        private System.Windows.Forms.TextBox textBoxd12;
+        private System.Windows.Forms.TextBox textBoxd20;
+        private System.Windows.Forms.TextBox textBoxPercentile;
+        private System.Windows.Forms.TextBox textBoxResults;
+        private System.Windows.Forms.Button buttonRoll;
+    }
+}
+
---
+++ DiceRollerExtreme/DiceRollerExtreme/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceRollerExtreme
+{
+    public partial class Form1 : Form
+    {
+        int totalInt = 0;
+        int[] diceArray = new int[7];
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private  int rollDice(int amount, int type)
+        {
+            int totalAmount = 0;
+            for(int i = 0; i < amount; i++)
+            {
+                totalAmount += random.Next(1, type + 1);
+            }
+            return totalAmount;
+        }
+        private void buttonRoll_Click(object sender, EventArgs e)
+        {
+            totalInt = 0;
+            if (textBoxd4.Text == "")
+            {
+                diceArray[0] = 0;
+            }
+            else
+            {
+                diceArray[0] = int.Parse(textBoxd4.Text);
+            }
+            if (textBoxd6.Text == "")
+            {
+                diceArray[1] = 0;
+            }
+            else
+            {
+                diceArray[1] = int.Parse(textBoxd6.Text);
+            }
+            if (textBoxd8.Text == "")
+            {
+                diceArray[2] = 0;
+            }
+            else
+            {
+                diceArray[2] = int.Parse(textBoxd8.Text);
+            }
+            if (textBoxd10.Text == "")
+            {
+                diceArray[3] = 0;
+            }
+            else
+            {
+                diceArray[3] = int.Parse(textBoxd10.Text);
+            }
+            if (textBoxd12.Text == "")
+            {
+                diceArray[4] = 0;
+            }
+            else
+            {
+                diceArray[4] = int.Parse(textBoxd12.Text);
+            }
+            if (textBoxd20.Text == "")
+            {
+                diceArray[5] = 0;
+            }
+            else
+            {
+                diceArray[5] = int.Parse(textBoxd20.Text);
+            }
+            if (textBoxPercentile.Text == "")
+            {
+                diceArray[6] = 0;
+            }
+            else
+            {
+                diceArray[6] = int.Parse(textBoxPercentile.Text);
+            }
+            totalInt += rollDice(diceArray[0], 4);
+            totalInt += rollDice(diceArray[1], 6);
+            totalInt += rollDice(diceArray[2], 8);
+            totalInt += rollDice(diceArray[3], 10);
+            totalInt += rollDice(diceArray[4], 12);
+            totalInt += rollDice(diceArray[5], 20);
+            totalInt += rollDice(diceArray[6], 100);
+            textBoxResults.Text = totalInt.ToString();
+        }
+    }
+}
